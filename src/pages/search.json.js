4f79541fe1bfb3289a9308f9a95605736ecb5fb3
BFladderbeanqwq <139599235+BFladderbeanawa@users.noreply.github.com>
2026-02-08 20:59:00 +0800

    Code: Init design

---
+++ src/pages/search.json.js
@@ -0,0 +1,19 @@
+// src/pages/search.json.js
+import { getCollection } from 'astro:content';
+
+export async function GET({}) {
+	const posts = await getCollection('blog');
+	const body = JSON.stringify(
+		posts.map((post) => ({
+			title: post.data.title,
+			description: post.data.description,
+			slug: post.slug,
+            tags: post.data.tags
+		}))
+	);
+	return new Response(body, {
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	});
+}
